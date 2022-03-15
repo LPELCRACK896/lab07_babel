3a3ede851d66fcb6b4288a58dbfcb5f5b42731b6
LPELCRACK896 <60456157+LPELCRACK896@users.noreply.github.com>
2022-03-15 11:31:04 -0600

    webpack install

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-import { newBook } from "../create-book/newBook"
+import { newBook } from "./create-book/newBook"
 export const startApp  = () =>{
     for (var i = 4; i < 14; i++){
         newBook("container_"+i)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import { startApp } from "./app/app";
 import './CSS/index_style.css'
-import  'bootstrap'
 startApp()
